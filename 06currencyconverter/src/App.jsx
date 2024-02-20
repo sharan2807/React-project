922d7106e1f3ea1868c7fb96e5ebb1d6aa9ba556
sharan2807 <sharancold98@gmail.com>
2024-02-20 12:42:16 +0530

    Adding background image

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`,
+        backgroundImage: `url(https://img.freepik.com/free-vector/digital-global-currency-icons-concept-background_1017-17455.jpg)`,
       }}
     >
       <h1 className="bg-red-200">Test for Tailwind</h1>
