29e94e47408dbc55cdbcd6f1d608ba62ebcc4946
sharan2807 <sharancold98@gmail.com>
2024-02-20 12:39:37 +0530

    Adding background image

---
+++ 06currencyconverter/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`,
+      }}
+    >
+      <h1 className="bg-red-200">Test for Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
